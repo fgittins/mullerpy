682dd3cb8ffbaee753f0deb748596b255068a17a
Fabian Gittins <fabiangittins@gmail.com>
2025-06-10 11:54:28 +0200

    Fix `__version__`

--- src/mullerpy/__init__.py
+++ src/mullerpy/__init__.py
@@ -1,6 +1,8 @@
 """A Python implementation of Muller's method."""
 
+from importlib.metadata import version
+
 from .muller import muller
 
 __all__ = ["muller"]
-__version__ = "0.1.0"
+__version__ = version("mullerpy")
