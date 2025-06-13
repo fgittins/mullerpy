418a124e1a8d5786d4bccf7197fce9b9bda91dbe
Fabian Gittins <fabiangittins@gmail.com>
2025-06-13 17:35:56 +0200

    Change function definitions for `args` testing

--- tests/test_muller.py
+++ tests/test_muller.py
@@ -5,22 +5,30 @@ import numpy
 from mullerpy import muller
 
 
-def f(x: complex, n: int = 2, p: int = 612) -> complex:
+def f(x: complex, n: complex, p: complex) -> complex:
     return x**n - p
 
 
-def g(x: float) -> float:
+def g(x: complex) -> complex:
+    return f(x, 2, 612)
+
+
+def h(x: complex) -> complex:
+    return f(x, 3, 1)
+
+
+def i(x: complex) -> complex:
     return numpy.exp(-x) * numpy.sin(x)
 
 
 class TestMuller(TestCase):
     def test_quadratic_roots(self) -> None:
         x = (10, 20, 30)
-        res = muller(f, x)
+        res = muller(g, x)
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
         x = (-10, -20, -30)
-        res = muller(f, x)
+        res = muller(g, x)
         self.assertAlmostEqual(res.root, -(612 ** (1 / 2)), delta=1e-5)
 
     def test_sine_roots(self) -> None:
@@ -34,15 +42,15 @@ class TestMuller(TestCase):
 
     def test_exp_sine_roots(self) -> None:
         x = (-2, -3, -4)
-        res = muller(g, x)
+        res = muller(i, x)
         self.assertAlmostEqual(res.root, -numpy.pi, delta=1e-5)
 
         x = (-1, 0, 1 / 2)
-        res = muller(g, x)
+        res = muller(i, x)
         self.assertAlmostEqual(res.root, 0, delta=1e-5)
 
         x = (-1, 0, 1)
-        res = muller(g, x)
+        res = muller(i, x)
         self.assertAlmostEqual(res.root, numpy.pi, delta=1e-5)
 
     def test_args(self) -> None:
@@ -57,16 +65,14 @@ class TestMuller(TestCase):
         self.assertAlmostEqual(res.root, -(612 ** (1 / 2)), delta=1e-5)
 
     def test_complex_roots(self) -> None:
-        n, p = 3, 1
-
         x = (-1, (-1 + 1j) / 2, 1j)
-        res = muller(f, x, args=(n, p))
+        res = muller(h, x)
         self.assertAlmostEqual(
             res.root, (-1 + 3 ** (1 / 2) * 1j) / 2, delta=1e-5
         )
 
         x = (-1, -(1 + 1j) / 2, -1j)
-        res = muller(f, x, args=(n, p))
+        res = muller(h, x)
         self.assertAlmostEqual(
             res.root, -(1 + 3 ** (1 / 2) * 1j) / 2, delta=1e-5
         )
@@ -74,11 +80,11 @@ class TestMuller(TestCase):
     def test_tol(self) -> None:
         x = (10, 20, 30)
 
-        res = muller(f, x, xtol=0)
+        res = muller(g, x, xtol=0)
         self.assertTrue(res.converged)
         self.assertIn("function", res.flag)
 
-        res = muller(f, x, ftol=0)
+        res = muller(g, x, ftol=0)
         self.assertTrue(res.converged)
         self.assertIn("root", res.flag)
 
@@ -86,36 +92,36 @@ class TestMuller(TestCase):
         x = (10, 20)
 
         with self.assertRaises(ValueError):
-            muller(f, x)
+            muller(g, x)
 
         x = (10, 20, 30)
 
         with self.assertRaises(ValueError):
-            muller(f, x, xtol=-1e-5)
+            muller(g, x, xtol=-1e-5)
 
         with self.assertRaises(ValueError):
-            muller(f, x, ftol=-1e-5)
+            muller(g, x, ftol=-1e-5)
 
         with self.assertRaises(ValueError):
-            muller(f, x, maxiter=0)
+            muller(g, x, maxiter=0)
 
     def test_iterable(self) -> None:
         n, p = 2, 612
 
         x = [10, 20, 30]
-        res = muller(f, x, args=(n, p))
+        res = muller(g, x)
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
         x = {10, 20, 30}
-        res = muller(f, x, args=(n, p))
+        res = muller(g, x)
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
         x = range(10, 31, 10)
-        res = muller(f, x, args=(n, p))
+        res = muller(g, x)
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
         x = numpy.array([10, 20, 30], dtype=numpy.int64)
-        res = muller(f, x, args=(n, p))
+        res = muller(g, x)
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
         x = (10, 20, 30)
