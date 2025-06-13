6d9793a914228c554c5e12df1c7cd2d57229fbd5
Fabian Gittins <fabiangittins@gmail.com>
2025-06-13 20:55:35 +0200

    Use `mypy` for type checking

--- tests/test_muller.py
+++ tests/test_muller.py
@@ -1,3 +1,4 @@
+from typing import cast
 from unittest import TestCase
 
 import numpy
@@ -18,7 +19,7 @@ def h(x: complex) -> complex:
 
 
 def i(x: complex) -> complex:
-    return numpy.exp(-x) * numpy.sin(x)
+    return cast("complex", numpy.exp(-x) * numpy.sin(x))
 
 
 class TestMuller(TestCase):
@@ -45,12 +46,12 @@ class TestMuller(TestCase):
         res = muller(i, x)
         self.assertAlmostEqual(res.root, -numpy.pi, delta=1e-5)
 
-        x = (-1, 0, 1 / 2)
-        res = muller(i, x)
+        y = (-1, 0, 1 / 2)
+        res = muller(i, y)
         self.assertAlmostEqual(res.root, 0, delta=1e-5)
 
-        x = (-1, 0, 1)
-        res = muller(i, x)
+        z = (-1, 0, 1)
+        res = muller(i, z)
         self.assertAlmostEqual(res.root, numpy.pi, delta=1e-5)
 
     def test_args(self) -> None:
@@ -94,43 +95,43 @@ class TestMuller(TestCase):
         with self.assertRaises(ValueError):
             muller(g, x)
 
-        x = (10, 20, 30)
+        y = (10, 20, 30)
 
         with self.assertRaises(ValueError):
-            muller(g, x, xtol=-1e-5)
+            muller(g, y, xtol=-1e-5)
 
         with self.assertRaises(ValueError):
-            muller(g, x, ftol=-1e-5)
+            muller(g, y, ftol=-1e-5)
 
         with self.assertRaises(ValueError):
-            muller(g, x, maxiter=0)
+            muller(g, y, maxiter=0)
 
     def test_iterable(self) -> None:
         n, p = 2, 612
 
-        x = [10, 20, 30]
-        res = muller(g, x)
+        v = [10, 20, 30]
+        res = muller(g, v)
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
-        x = {10, 20, 30}
-        res = muller(g, x)
+        w = {10, 20, 30}
+        res = muller(g, w)
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
         x = range(10, 31, 10)
         res = muller(g, x)
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
-        x = numpy.array([10, 20, 30], dtype=numpy.int64)
-        res = muller(g, x)
+        y = numpy.array([10, 20, 30], dtype=numpy.int64)
+        res = muller(g, y)
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
-        x = (10, 20, 30)
+        z = (10, 20, 30)
 
-        res = muller(f, x, args=[n, p])
+        res = muller(f, z, args=[n, p])
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
-        res = muller(f, x, args={n, p})
+        res = muller(f, z, args={n, p})
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
 
-        res = muller(f, x, args=numpy.array([n, p], dtype=numpy.int64))
+        res = muller(f, z, args=numpy.array([n, p], dtype=numpy.int64))
         self.assertAlmostEqual(res.root, 612 ** (1 / 2), delta=1e-5)
